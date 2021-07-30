e1b2abab700a34e84d104cadb2e9a8d83d48320d
abrarzahed <abrarzahed6986@gmail.com>
2021-07-30 15:01:52 +0600

    added google font to global style file

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,26 @@
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  head() {
+    return {
+      title: "OC Academy",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best shoe store"
+        }
+      ],
+      link: []
+    };
+  }
+};
+</script>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+/* @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"); */
 * {
   margin: 0;
   padding: 0;
@@ -18,7 +34,7 @@
 body {
   line-height: 1.5;
   background: #fafddc;
-  font-family: "Quicksand", sans-serif;
+  /* font-family: "Quicksand", sans-serif; */
   overflow-x: hidden;
 }
 .app {
